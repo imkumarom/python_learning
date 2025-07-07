e40e4d0575b9199f98a7a1a9d90cefd21d43522e
Kumar om <kumarom@MacBookAir.lan>
2025-07-07 22:55:50 +0530

    today coding

--- 20_check_armstrong_num.py
+++ 20_check_armstrong_num.py
@@ -1,3 +1,15 @@
-num = int(input("enter number:"))
 
+for num in range(1,1000000001):
+    actual_no=num
+    cube_sum_val=0
+    power=len(str(num))
 
+    while (num>0):
+        last_dist=num%10
+        cube_sum_val=last_dist ** power + cube_sum_val
+        num=int(num/10)
+
+    if actual_no==cube_sum_val:
+        print(f"{actual_no} number is armstrong")
+# else:
+#     print(f"{actual_no} number is not armstrong")
