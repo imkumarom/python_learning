e26f682fff93fc49219d779c19e546f2f9ebf077
Kumar om <kumarom@MacBookAir.lan>
2025-07-08 01:10:03 +0530

    new code

--- 100_revision.py
+++ 100_revision.py
@@ -1,1 +1,15 @@
-from loguru import logger
+# for num in range(2,1000000000):
+#     jojo = 0
+#     for i in range(1,num+1):
+#         if num % i == 0:
+#             jojo = jojo + 1
+         
+#     if jojo <= 2:
+#        print(f"{num} is prime")
+#     # print(num,jojo)
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
